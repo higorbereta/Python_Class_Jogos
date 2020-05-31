2b98a8f69cbe8b4d71f26b03a51ecd1482a6feab
Higor Bereta <higor.bereta@gmail.com>
2020-05-31 11:41:45 -0300

    Class 5

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,16 +4,20 @@ print("*********************************")
 
 numero_secreto = 42
 total_tentativas = 3
-rodada = 1
 
 #chute_str = input("Digite seu número: ")
 #chute = int(chute_str)
 
-while(rodada <= total_tentativas):
+for rodada in range (1, total_tentativas + 1):
     print("Tentativas {} de {}: ".format(rodada, total_tentativas))
-    chute = int(input("Digite seu número: "))
+    chute = int(input("Digite um número entre 1 e 100: "))
     print("*********************************")
     print("Voce digitou ", chute)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        print("*********************************")
+        continue
 
     acertou = numero_secreto == chute #Variável booleana
     maior = chute > numero_secreto
@@ -21,7 +25,7 @@ while(rodada <= total_tentativas):
 
     if (acertou):
         print("Voce acertou!")
-        rodada = 4
+        break
     else:
         if(maior):
             print("Você errou! O seu chute foi maior do que o número secreto.")
@@ -29,6 +33,5 @@ while(rodada <= total_tentativas):
         elif (menor):
             print("Você errou! O seu chute foi menor do que o número secreto.")
             print("*********************************")
-    rodada = rodada +1
 print("Fim do jogo")
 
